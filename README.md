ea3d22f10520adca45eff0a7fa90e2c48968bdba
aadneu <141396377+aadneu@users.noreply.github.com>
2023-08-31 11:35:36 +0200

    Add files via upload
    dag 3

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 Get Academy gruppeprosjekt for uke 4
 
 [Link til github pages](https://tobiasrefsholt.github.io/Gruppeoppgave--de--y/)
+
+
+// lage forskjellige bilder av greta der hun blir gradvis mer grønn/gul av skjørbuk
+// Gjøre appelsin og pumpe klikkbare gjennom havet
+// lage global oppvarming-overskrift
+// gjøre scurvy til lose-condition.
--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 
 const app = document.getElementById('app')
 let seaLevel = 20;
+let scurvy = 0;
+let lastSeaLevel;
+let lastScurvy;
 
 
 // View
@@ -11,10 +14,17 @@ updateView();
 function updateView() {
     app.innerHTML = /*html*/`
     <div class="grid">
+    <div id="header"> Fix Global Warming!</div>
         <div class="sea" style="height: ${seaLevel}%"></div>
-        <div id="greta"></div>
-        <div id="oranges" onclick="eat()"></div>
-        <div id="pump" onclick="pump()"></div>
+        <div>My scurvy is: ${scurvy} </div>
+        <div id="pictures">
+            <div id="oranges" onclick="eat()"></div>
+            <div id="greta"></div>
+            <div id="snorkel"></div>
+            <div id="howdare"></div>
+            <div id="pump" onclick="pump()"></div>
+        </div>
+
     </div>
     <div></div>
     <div></div>
@@ -24,21 +34,49 @@ function updateView() {
 
 // Controller
 
+let timerSea = setInterval(increaseSeaLevel, 500);
+let timerScurvy = setInterval(increaseScurvy, 2500);
+
 function increaseSeaLevel() {
-    (seaLevel > 50) && gameOver();
-    seaLevel += 5;
+    if (seaLevel >= 67) {
+        gameOver();
+    } else {
+    lastSeaLevel = seaLevel;
+    seaLevel += 1;
+}
     updateView();
 }
-let timer = setInterval(increaseSeaLevel, 5000);
 
-function gameOver() {
 
+function increaseScurvy() {
+    if (scurvy >= 100) {
+        gameOver();
+    } else {
+        lastScurvy = scurvy;
+        scurvy += 10;
+    }
+    updateView();
 }
 
-function eat(){
-
+function eat() {
+    scurvy = 0;
+    updateView();
 }
 
 function pump(){
-    
+    seaLevel += -3;
+    updateView();
+}
+
+function gameOver() {
+    if(seaLevel >= 67 && lastSeaLevel < 67){
+        alert("You drowned");
+        updateView();
+        return;
+    }
+    if (scurvy >= 100 && lastScurvy < 100){
+        alert("You died of scurvy");
+        updateView();
+        return;
+    }
 }
